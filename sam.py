73ae9970b8c0385e96b10f5a83a141272673c5a7
Charlie Godfrey <cgodfrey@uw.edu>
2023-07-07 01:34:49 -0500

    gah, trying to make handlers ...

---
+++ sam.py
@@ -0,0 +1,51 @@
+from segment_anything.modeling import Sam, ImageEncoderViT, PromptEncoder, MaskDecoder, TwoWayTransformer,
+
+# for now doing vit_b version
+
+def Model(Sam):
+    def __init__(self,
+                prompt_embed_dim: int = 256,
+                image_size:int=1024,
+                vit_patch_size:int=16,
+                # vit_b params
+                encoder_embed_dim=1024,
+                encoder_depth=24,
+                encoder_num_heads=16,
+                encoder_global_attn_indexes=[5, 11, 17, 23],):
+        image_embedding_size = image_size / vit_patch_size
+        super().__init__(
+            image_encoder=ImageEncoderViT(
+                depth=encoder_depth,
+                embed_dim=encoder_embed_dim,
+                img_size=image_size,
+                mlp_ratio=4,
+                norm_layer=partial(torch.nn.LayerNorm, eps=1e-6),
+                num_heads=encoder_num_heads,
+                patch_size=vit_patch_size,
+                qkv_bias=True,
+                use_rel_pos=True,
+                global_attn_indexes=encoder_global_attn_indexes,
+                window_size=14,
+                out_chans=prompt_embed_dim,
+            ),
+            prompt_encoder=PromptEncoder(
+                embed_dim=prompt_embed_dim,
+                image_embedding_size=(image_embedding_size, image_embedding_size),
+                input_image_size=(image_size, image_size),
+                mask_in_chans=16,
+            ),
+            mask_decoder=MaskDecoder(
+                num_multimask_outputs=3,
+                transformer=TwoWayTransformer(
+                    depth=2,
+                    embedding_dim=prompt_embed_dim,
+                    mlp_dim=2048,
+                    num_heads=8,
+                ),
+                transformer_dim=prompt_embed_dim,
+                iou_head_depth=3,
+                iou_head_hidden_dim=256,
+            ),
+            pixel_mean=[123.675, 116.28, 103.53],
+            pixel_std=[58.395, 57.12, 57.375],
+        )  # acutally add all the args we want / load state dict??
---
+++ sam_handler.py
@@ -0,0 +1,76 @@
+from segment_anything import SamPredictor, SamAutomaticMaskGenerator
+from ts.torch_handler.base_handler import BaseHandler
+import torch
+import base64
+import io
+from abc import ABC
+
+import torch
+from PIL import Image
+
+from torchvision import transforms as T
+import numpy as np
+
+class ModelHandler(BaseHandler):
+    """
+    A custom model handler implementation.
+    """
+
+    def __init__(self):
+        super().__init__()
+        # actually sam code uses np arrays
+        # self.image_processing = T.Compose(
+        #     [
+        #         T.Resize(1440),
+        #         T.CenterCrop(1024),
+        #         T.ToTensor(),
+        #         # handled by sam itself
+        #         # T.Normalize(
+        #         #     mean=[0.485, 0.456, 0.406],
+        #         #     std=[0.229, 0.224, 0.225]
+        #         # )
+        #     ]
+        # )
+
+    def preprocess(self, data):
+        # copied from visionhandler
+        images = []
+
+        for row in data:
+            # Compat layer: normally the envelope should just return the data
+            # directly, but older versions of Torchserve didn't have envelope.
+            image = row.get("data") or row.get("body")
+            if isinstance(image, str):
+                # if the image is a string of bytesarray.
+                image = base64.b64decode(image)
+
+            # If the image is sent as bytesarray
+            if isinstance(image, (bytearray, bytes)):
+                image = Image.open(io.BytesIO(image))
+                # image = self.image_processing(image)
+                image = np.asarray(image)
+
+            else:
+                # if the image is a list
+                image = torch.FloatTensor(image)
+
+            images.append(image)
+
+        return torch.stack(images).to(self.device)
+    
+    def inference(self, data, *args, **kwargs):
+        """
+        The Inference Function is used to make a prediction call on the given input request.
+        The user needs to override the inference function to customize it.
+
+        Args:
+            data (Torch Tensor): A Torch Tensor is passed to make the Inference Request.
+            The shape should match the model input shape.
+
+        Returns:
+            Torch Tensor : The Predicted Torch Tensor is returned in this function.
+        """
+        mg = SamAutomaticMaskGenerator(self.model)
+        with torch.no_grad():
+            results = mg.generate(data, *args, **kwargs)
+        return results
