a14d3ffb35134edb1355095c5c08a861b245001f
Charlie Godfrey <cgodfrey@uw.edu>
2023-07-07 00:23:09 -0500

    adding segment anything submod

--- serve_env.sh
+++ serve_env.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-# # automatically install miniconda, dependencies
-# mkdir ~/MiniCondaInstall && cd ~/MiniCondaInstall;
-# curl -o install.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh;
-# bash install.sh -b -p $HOME/miniconda3;
-# source ~/.bashrc;
-# conda create --name=ts python=3.9;
-# conda activate ts;
+# install torchserve + dependencies
 cd ~/samserve/serve;
-python ts_scripts/install_dependencies.py --cuda=cu117; #need to figure out how to deal w/ sudo ...
+python ts_scripts/install_dependencies.py --cuda=cu117; # vm actually has 12.0 ...
 pip install torchserve torch-model-archiver torch-workflow-archiver;
+
+# install segment-anything
+cd ~/samserve/segment-anything;
+
+
+# launch torchserve
+cd ~;
+mkdir models && mkdir workflows;
+torchserve --start --model-store=models --workflow-store=workflows;
