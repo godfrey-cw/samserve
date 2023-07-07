05aabd6df32f1c5e6d5937b39c6ff5f6a861528d
Charlie Godfrey <cgodfrey@uw.edu>
2023-07-07 03:38:27 -0500

    trying to add ssl

--- serve_env.sh
+++ serve_env.sh
@@ -13,7 +13,12 @@ pip install -e .;
 cd ~;
 mkdir models && mkdir samweights;
 curl -o samweights/sam_vit_b_01ec64.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth;
-torch-model-archiver --model-name sam-auto-maskgen --serialized-file samweights/sam_vit_b_01ec64.pth --model-file samserve/sam.py --handler samserve/sam_handler.py  --export-path models/sam-auto-maskgen.mar -v 0.1;
+torch-model-archiver --model-name sam-auto-maskgen --serialized-file samweights/sam_vit_b_01ec64.pth --model-file samserve/sam.py --handler samserve/sam_auto_maskgen.py  --export-path models/sam-auto-maskgen.mar -v 0.1;
+
+# ssl stuff (note, requires interactive)
+cd ~;
+mkdir keys;
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout keys/mykey.key -out keys/mycert.pem;
 
 # launch torchserve
-torchserve --model-store /models --start --models all --no-config-snapshots;
+torchserve --model-store /models --start --models all --ts-config samserve/config.properties --no-config-snapshots;
