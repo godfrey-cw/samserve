0530823f4bc13e2e1dfb090373ceaf842d685e8f
Charlie Godfrey <cgodfrey@uw.edu>
2023-07-10 04:56:09 -0500

    more models/handlers

---
+++ predict.py
@@ -0,0 +1,104 @@
+from segment_anything import SamPredictor, SamAutomaticMaskGenerator
+from ts.torch_handler.base_handler import BaseHandler
+import torch
+import base64
+import io
+from abc import ABC
+
+import torch
+from PIL import Image
+
+from torchvision import transforms as T
+import numpy as np
+
+
+class ModelHandler(BaseHandler):
+    """
+    A custom model handler implementation.
+    """
+
+    def __init__(self):
+        super().__init__()
+        # actually sam code uses np arrays
+        # self.image_processing = T.Compose(
+        #     [
+        #         T.Resize(1440),
+        #         T.CenterCrop(1024),
+        #         T.ToTensor(),
+        #         # handled by sam itself
+        #         # T.Normalize(
+        #         #     mean=[0.485, 0.456, 0.406],
+        #         #     std=[0.229, 0.224, 0.225]
+        #         # )
+        #     ]
+        # )
+
+    def preprocess(self, data):
+        # copied from visionhandler
+        images = []
+        for row in data:
+            # Compat layer: normally the envelope should just return the data
+            # directly, but older versions of Torchserve didn't have envelope.
+            print(row)
+            # image = row.get("body") or row.get("data")
+            # d = row.get("body") or row.get("data")
+            # image = d["image"]
+            image = row["image"]
+            prompt = row["prompt"]
+            prompt = prompt.getvalue().decode("utf-8")
+            if isinstance(image, str):
+                # if the image is a string of bytesarray.
+                image = base64.b64decode(image)
+
+            # If the image is sent as bytesarray
+            if isinstance(image, (bytearray, bytes)):
+                image = Image.open(io.BytesIO(image))
+                # image = self.image_processing(image)
+                image = np.asarray(image)
+
+            else:
+                # if the image is a list
+                image = torch.FloatTensor(image)
+
+            images.append([image, prompt])
+
+        # current inferrence code expects a single np array
+        return images
+
+    def inference(self, data, *args, **kwargs):
+        """
+        The Inference Function is used to make a prediction call on the given input request.
+        The user needs to override the inference function to customize it.
+
+        Args:
+            data (Torch Tensor): A Torch Tensor is passed to make the Inference Request.
+            The shape should match the model input shape.
+
+        Returns:
+            Torch Tensor : The Predicted Torch Tensor is returned in this function.
+        """
+        sp = SamPredictor(self.model)
+        results = []
+        with torch.no_grad():
+            for row in data:
+                image, prompt = row
+                sp.set_image(image)
+                out = sp.predict(prompt)
+                result = [[x.tolist() for x in o] for o in out]
+                results.append(result)
+        return results
+
+    def postprocess(self, data):
+        """
+        The post process function makes use of the output from the inference and converts into a
+        Torchserve supported response output.
+
+        Args:
+            data (Torch Tensor): The torch tensor received from the prediction output of the model.
+
+        Returns:
+            List: The post process function returns a list of the predicted output.
+        """
+
+        # return data.tolist()
+        return data
--- serve_env.sh
+++ serve_env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt update && sudo apt upgrade;
+
 # install torchserve + dependencies
 cd ~/samserve/serve;
 python ts_scripts/install_dependencies.py --cuda=cu117; # vm actually has 12.0 ...
@@ -13,8 +14,19 @@ pip install -e .;
 # make model archive file
 cd ~;
 mkdir models && mkdir samweights;
-curl -o samweights/sam_vit_b_01ec64.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth;
-torch-model-archiver --model-name sam-auto-maskgen --serialized-file samweights/sam_vit_b_01ec64.pth --model-file samserve/sam.py --handler samserve/sam_auto_maskgen.py  --export-path models -v 0.1;
+curl -o samweights/sam_b.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth;
+curl -o samweights/sam_l.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth;
+curl -o samweights/sam_h.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth;
+
+for m in {'sam_b','sam_l','sam_h'}; do
+    for h in {'auto_maskgen','predict'}; do
+        torch-model-archiver --model-name $m'_'$h \
+            --serialized-file 'samweights/'$m'.pth' \
+            --model-file 'samserve/'$m'.py' \
+            --handler 'samserve/'$h'.py' \
+            --export-path models -v 0.1;
+    done;
+done
 
 # ssl stuff (note, requires interactive)
 cd ~;
